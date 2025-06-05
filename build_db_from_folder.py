dee4f5648188a0562680a671304d022dc173a7ab
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-06-05 14:10:11 +0200

    refactor: streamline database building process and improve alias extraction

--- build_db_from_folder.py
+++ build_db_from_folder.py
@@ -3,15 +3,28 @@ import os
 
 import pandas as pd
 
-from csv_processing import filename_processing
 from modules.db_connection import connect_to_db
+from modules.filename_processing import extract_filename_parts
+from modules.tables_metadata import tables_metadata
 
 INPUTS_FOLDER = "data"
+# TABLE_FORMAT = "CREL"  # relational CSV
+# TABLE_FORMAT = "CTAB"  # tabular CSV (multi-dimensional)
 
 
 def list_csv_files(folder):
-    """List all CSV files in the specified folder."""
+    """List CSV files in folder."""
     return [file for file in os.listdir(folder) if file.endswith(".csv")]
+
+
+def get_table_alias(file, table_format):
+    """Return table alias for file."""
+    parts = extract_filename_parts(file, table_format=table_format)
+    if parts and parts["group"] in tables_metadata:
+        table_type = parts["table_type"]
+        table_alias = tables_metadata[parts["group"]]["alias"]
+        return f"{table_type}_{table_alias}"
+    return None
 
 
 def open_file_as_df(file_path):
@@ -20,23 +33,16 @@ def open_file_as_df(file_path):
     return pd.read_csv(file_path, sep=";")
 
 
-def build_db_from_folder():
-    """Process all CSV files in the specified folder."""
-    csv_list = list_csv_files(INPUTS_FOLDER)
-
-    for file in csv_list:
-        file_df = open_file_as_df(file)
-        filename_parts = filename_processing.extract_filename_parts(file)
-        if filename_parts:
-            first = filename_parts["first_four_letters"]
-            subgroup = filename_parts["subgroup"]
-            table_id = f"{filename_parts['table_id']}"
-            table_name = f"{first}_{subgroup}_{table_id}"
-            file_df.to_sql(table_name, con=connect_to_db(), if_exists="replace", index=False)
-            # print(f"Table {table_name} created from {file}")
-            print(f"Table {table_name} added to the database from {file}")
-        else:
-            print(f"Filename {file} does not match expected format.")
+def build_db_from_folder(table_format=None):
+    """Build database from CSV files in folder."""
+    for file in list_csv_files(INPUTS_FOLDER):
+        alias = get_table_alias(file, table_format)
+        if not alias:
+            print(f"Skipping file {file}: no table metadata found.")
+            continue
+        df = open_file_as_df(file)
+        df.to_sql(alias, con=connect_to_db(), if_exists="replace", index=False)
+        print(f"Table {alias} added to the database from {file}")
 
 
 if __name__ == "__main__":
