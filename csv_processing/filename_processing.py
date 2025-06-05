f97cc3782db498e382f5ae394b4eb00661c660c0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-06-05 13:16:49 +0200

    refactor: update filename extraction logic - enhance extract_filename_parts to include table_format validation - streamline get_table_ids_from_folder for clarity

--- csv_processing/filename_processing.py
+++ csv_processing/filename_processing.py
@@ -1,27 +1,58 @@
+import os
 import re
 
 
-def extract_filename_parts(filename):
+def extract_filename_parts(filename, table_format=None):
     """Extract parts from the filename."""
     four_letters = r"([A-Z]{4})"
-    subgroup = r"(\d+)"
+    group = r"(\d+)"
     date = r"(\d{8})"
     table_id = r"(\d{6})"
-    pattern = f"^{four_letters}_{subgroup}_{four_letters}_{date}{table_id}.csv$"
+    pattern = f"^{four_letters}_{group}_{four_letters}_{date}{table_id}.csv$"
     match = re.match(pattern, filename)
 
     if match:
-        first_four_letters = match.group(1)
-        subgroup = match.group(2)
-        second_four_letters = match.group(3)
+        category = match.group(1)
+        group = match.group(2)
+        table_type = match.group(3)
         date = match.group(4)
         table_id = match.group(5)
 
+        table_format_mismatch = table_format and table_type != table_format
+        if table_format_mismatch:
+            return None
+
         return {
-            "first_four_letters": first_four_letters,
-            "subgroup": subgroup,
-            "second_four_letters": second_four_letters,
+            "category": category,
+            "group": group,
+            "table_type": table_type,
             "date": date,
             "table_id": table_id,
         }
     return None
+
+
+def get_table_ids_from_folder(folder, table_format=None):
+    """Get a list of table names from CSV files in the specified folder.\n
+    **Available formats:**\n
+    **'CTAB'** - (tabular)\n
+    **'CREL'** - (relational)."""
+    table_ids = []
+    for file in os.listdir(folder):
+        if file.endswith(".csv"):
+            filename_parts = extract_filename_parts(file, table_format=table_format)
+            if filename_parts:
+                group = filename_parts["group"]
+                table_type = filename_parts["table_type"]
+                table_id = f"{filename_parts['table_id']}"
+
+                table_ids.append(f"{group}_{table_type}_{table_id}")
+    return table_ids
+
+
+if __name__ == "__main__":
+    folder_path = ".\\data"
+    table_ids = get_table_ids_from_folder(folder_path)
+    print("Table IDs found in the folder:")
+    for table_id in table_ids:
+        print(table_id)
