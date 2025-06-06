26a4e2707aede7b413441b735a154649517b6fce
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-06-06 13:49:10 +0200

    feature: moved database to www.freesqldatabase.com

--- modules/db_connection.py
+++ modules/db_connection.py
@@ -1,12 +1,35 @@
 import sqlite3
 
+from sqlalchemy import create_engine
+
 DB_FILE = "database.db"
 
 
+class db_config:
+    # def __init__(self):
+    import os
+
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    HOST = os.getenv("db_host")
+    NAME = os.getenv("db_name")
+    USER = os.getenv("db_user")
+    PASSWORD = os.getenv("db_password")
+    PORT = os.getenv("db_port")
+
+
+# def connect_to_db():
+#     """Connect to the SQLite database."""
+#     conn = sqlite3.connect(DB_FILE)
+#     return conn
+
+
 def connect_to_db():
-    """Connect to the SQLite database."""
-    conn = sqlite3.connect(DB_FILE)
-    return conn
+    """Connect to the external database."""
+    db = db_config()
+    engine = create_engine(f"mysql+pymysql://{db.USER}:{db.PASSWORD}@{db.HOST}:{db.PORT}/{db.NAME}")
+    return engine.connect()
 
 
 def create_table_from_df(df, table_name):
@@ -19,3 +42,11 @@ def create_table_from_df(df, table_name):
 
     conn.commit()
     conn.close()
+
+
+if __name__ == "__main__":
+    # Example usage
+    conn = connect_to_db()
+    print("Connected to the database successfully.")
+    conn.close()
+    print("Connection closed.")
