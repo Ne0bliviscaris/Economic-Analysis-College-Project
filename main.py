13fe63c8dec0e8b7427abe953ccbab1bb129bc3b
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-06-07 10:09:58 +0200

    refactor: simplified main

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from build_db_from_folder import build_db_from_folder
 
-
-def main():
-    """Main function to build the database from CSV files."""
-    build_db_from_folder()
-    print("Database built successfully from CSV files.")
-
-
-if __name__ == "__main__":
-    main()
-    print("Database has been built from the CSV files in the folder.")
+build_db_from_folder()
+print("Database built successfully from CSV files.")
